ea17b43b1810992c2e2b95f653cf78a07b214818
Rob Steele <robert@steelewoolsoftware.com>
2018-04-17 12:55:41 -0700

    steele-test-astropy.py now works with python3. steele-xml-withRadius changed to today's date.

--- steele-test-astropy.py
+++ steele-test-astropy.py
@@ -8,7 +8,7 @@ from astropy.time import Time
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
 
 m33 = SkyCoord.from_name('M33')
-print m33
+print (m33)
 
 bear_mountain = EarthLocation(lat=41.3*u.deg, lon=-74*u.deg, height=390*u.m)
 utcoffset = -4*u.hour  # Eastern Daylight Time
@@ -18,7 +18,7 @@ m33altaz = m33.transform_to(AltAz(obstime=time,location=bear_mountain))
 print("M33's Azimuth  = {0.az:.2}".format(m33altaz))
 print("M33's Altitude = {0.alt:.2}".format(m33altaz))
 
-print m33altaz
+print (m33altaz)
 
 
 
